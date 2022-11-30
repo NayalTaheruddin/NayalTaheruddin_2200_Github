e688141459ee126d97cc127aa62b6984cf6011bc
nayal <nayal@10.100.210.189>
2022-11-30 13:56:16 -0600

    PE7.1test

---
+++ NayalTaheruddin_2200_PE7_1/src/Exercise07_01.java
@@ -0,0 +1,10 @@
+import java.util.scanner;
+public class Exercise07_01 {
+
+	public static voic main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		int numberOfStudentsn= input.nextInt();
+	}
+}
